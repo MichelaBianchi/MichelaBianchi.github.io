356f60aef62c423e1fb6d814ae6d249bb3193cb5
Michela Bianchi <michela.bianchi@it.abb.com>
2025-04-26 16:41:30 +0200

    Notifications + icons

--- service-worker.js
+++ service-worker.js
@@ -59,3 +59,11 @@ self.addEventListener('push', event => {
   };
   event.waitUntil(self.registration.showNotification(title, options));
 });
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+      clients.openWindow(event.notification.data.url || '/')
+    );
+});
+  
